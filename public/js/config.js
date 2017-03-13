09c52747295c08ad610e4f914ec082410da8b8cb
dongtao <dongtao@xiaomi.com>
2017-03-13 11:51:38 +0800

    增加layui

--- public/js/config.js
+++ public/js/config.js
@@ -3,5 +3,15 @@ require.config({
     paths: {
         "jquery": "jquery.min",
         "vue": "vue",
+        "vue1": "vue1",
+        "layui": "/js/components/layui/layui"
+    },
+    map: {
+        "*": {
+            "css": 'css.min'
+        }
+    },
+    shim: {
+        "layui": ["css!/js/components/layui/css/layui.css"]
     }
 })
