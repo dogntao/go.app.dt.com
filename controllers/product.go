df97d112dacd1b67d2f8757f93cc7164ddb08b31
dt <dogntao@163.com>
2017-07-31 18:47:20 +0800

    产品更新

--- controllers/product.go
+++ controllers/product.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"encoding/json"
+	"fmt"
+	"reflect"
 
 	"go.app.dt.com/models"
 )
@@ -46,7 +48,35 @@ func (p *ProductController) Update() {
 		var listUpProductInfo []UpProductInfo
 		listJson := req.PostFormValue("list")
 		err := json.Unmarshal([]byte(listJson), &listUpProductInfo)
+		upProducts := make([]map[string]string, 0)
+		upProduct := make(map[string]string, 0)
+		inPurcases := make([]map[string]string, 0)
+		inPurcase := make(map[string]string, 0)
+		tName := ""
+		tVal := ""
+		if err == nil {
+			for _, val := range listUpProductInfo {
+				rT := reflect.TypeOf(val)
+				rV := reflect.ValueOf(val)
+				upProducts = make(map[string]string, 0)
+				for i := 0; i < rT.NumField(); i++ {
+					tName = rT.Field(i).Name
+					tVal = rV.Field(i).Interface()
+					if tName != "Purcase" {
+						upProduct[tName] = tVal
+					} else {
+						inPurca[tName] = tVal
+					}
+				}
+				upProducts = append(upProducts, upProduct)
+				inPurcases = append(inPurcases, inPurcase)
+			}
+		} else {
+			fmt.Println(err)
+		}
+		fmt.Println(upProducts)
+		fmt.Println(inPurcases)
 		// upProductInfo.idArr =
-		productModel.UpdateProducts(listUpProductInfo)
+		// productModel.UpdateProducts(listUpProductInfo)
 	}
 }
